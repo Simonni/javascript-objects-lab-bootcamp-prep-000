fee2a892b2a6d696e7948504a93bf99d34290538
Simon Kiflay <szkiflay@gmail.com>
2018-08-14 20:47:35 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -10,14 +10,17 @@ function destructivelyUpdateObjectWithKeyAndValue(recipes, key, value){
   return recipes;
 }
 
-
+// non-destructive
 function deleteFromObjectByKey(recipes, key){
-  delete recipes.key;
+  const simo= Object.assign({}, recipes);
+  delete simo[key];
+  return simo;
+ 
 
 }
 
 function destructivelyDeleteFromObjectByKey(recipes, key){
-  delete recipes.key;
+  delete recipes[key];
   return recipes;
 }
 
